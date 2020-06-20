7fa96c774ddf10f1a7c6e36c3d81853dba2ba3e7
Vladimir Vasic <vladimir@vasic.io>
2020-06-20 07:32:12 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,19 @@
+# Disable firewall
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
+# Disable antivirus real-time scanning
 Set-MpPreference -DisableRealtimeMonitoring $true
 
+# Disable Windows updates
 sc.exe stop wuauserv
 
 sc.exe config wuauserv start=disabled
+
+# Disable Cortana
+$path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"     
+    IF(!(Test-Path -Path $path)) {  
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "Windows Search" 
+    }  
+    Set-ItemProperty -Path $path -Name "AllowCortana" -Value 1  
+    #Restart Explorer to change it immediately     
+    Stop-Process -name explorer
