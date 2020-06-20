388c449f77b312b47b3975a4ee4243c185fdaad3
Vladimir Vasic <vladimir@vasic.io>
2020-06-20 07:43:17 -0700

    Update alp-setup.ps1

--- alp-setup.ps1
+++ alp-setup.ps1
@@ -1,13 +1,15 @@
 # Disable firewall
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Write-Host "Firewall Disabled"
 
 # Disable antivirus real-time scanning
 Set-MpPreference -DisableRealtimeMonitoring $true
+Write-Host "Real-time Scanning Windows Defender Disabled"
 
 # Disable Windows updates
 sc.exe stop wuauserv
-
 sc.exe config wuauserv start=disabled
+Write-Host "Windows Updates Disabled"
 
 # Disable Cortana
 $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"     
@@ -17,3 +19,4 @@ $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
     Set-ItemProperty -Path $path -Name "AllowCortana" -Value 1  
     #Restart Explorer to change it immediately     
     Stop-Process -name explorer
+Write-Host "Cortana Disabled"
